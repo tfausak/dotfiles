6edd1e990b74715c04200299d553417341d31caf
Taylor Fausak <taylor@fausak.me>
2013-02-06 13:56:31 -0600

    Add a gemrc for ignoring documentation.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 dir=$( pwd -P )
 cd ~
-dotfiles=( aprc bash_profile gitconfig gitignore vimrc )
+dotfiles=( aprc bash_profile gemrc gitconfig gitignore vimrc )
 for dotfile in ${dotfiles[@]}
 do
     ln -fsv "$dir/$dotfile" ".$dotfile"
