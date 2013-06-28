2c6f161c4f6a3db6bd4d2682b06f86e5c1e61525
Taylor Fausak <taylor@fausak.me>
2013-06-28 15:30:57 -0500

    Sublime Text 2 -> 3

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ do
     ln -fsv "$dir/$dotfile" ".$dotfile"
 done
 
-# Sublime Text 2
+# Sublime Text 3
 ln -fsv "$dir/Preferences.sublime-settings" \
-    "Library/Application Support/Sublime Text 2/Packages/User/Preferences.sublime-settings"
+    "Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
 
 mkdir -pv ~/.vim/{backup,tmp,undo}
