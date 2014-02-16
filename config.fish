d043a12da8c2b61fac48964c4ca3b4fe1db692b4
Taylor Fausak <taylor@fausak.me>
2014-02-16 13:16:14 -0600

    Add Cabal bin
    Also only add the directories to the path if they exist.

--- config.fish
+++ config.fish
@@ -1,3 +1,2 @@
-set fish_user_paths \
-    ~/bin \
-    $fish_user_path
+test -d ~/.cabal/bin; and set fish_user_paths ~/.cabal/bin $fish_user_paths
+test -d ~/bin; and set fish_user_paths ~/bin $fish_user_paths
